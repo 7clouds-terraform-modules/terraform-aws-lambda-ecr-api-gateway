5ec8016199d0bccf5901f50176401dc7d718f490
Yrwing Seven <irvi-seven@Irvi-PC.>
2025-09-18 12:55:06 -0300

    Uncommenting variables and reverting .env logic in main.tf

--- examples/cors-options/main.tf
+++ examples/cors-options/main.tf
@@ -17,15 +17,15 @@ module "lambda_ecr_api_gateway" {
   }
   ENABLE_OPTIONS_INTEGRATION = true
 
-  # Structural
-  # SECURITY_GROUP_IDS                  = ["YourSecurityGroupsID(s)Here"]
-  # SUBNET_IDS                          = ["YourSubnetsID(s)Here"]
-  # WARMUP_ENABLED                      = true
-  # LAMBDA_WARMUP_SCHEDULE_EXPRESSION   = "rate(5 minutes)"
+  Structural
+  SECURITY_GROUP_IDS                  = ["YourSecurityGroupsID(s)Here"]
+  SUBNET_IDS                          = ["YourSubnetsID(s)Here"]
+  WARMUP_ENABLED                      = true
+  LAMBDA_WARMUP_SCHEDULE_EXPRESSION   = "rate(5 minutes)"
   MEMORY_SIZE                         = 128
   TIMEOUT                             = 60
-  # ENVIRONMENT_VARIABLES               = { foo = "bar" }
-  # ATTACHED_POLICY_ARNS                 = ["YourAttachedPolic(y/ies)Here"]
+  ENVIRONMENT_VARIABLES               = { foo = "bar" }
+  ATTACHED_POLICY_ARNS                 = ["YourAttachedPolic(y/ies)Here"]
   API_GATEWAY_INTEGRATION_HTTP_METHOD = "POST"
   API_GATEWAY_INTEGRATION_INPUT_TYPE  = "AWS_PROXY"
   CORS_ALLOWED_ORIGINS                = "*"
